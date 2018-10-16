3a7e54affac685815a12dee9524329ef745b9b48
jch <jch@pfb24.cshl.edu>
2018-10-16 17:00:18 -0400

    added more tests

--- truthandlies.py
+++ truthandlies.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-ten = -10
+import sys
+ten = int(sys.argv[1])
 
 if ten < 0:
 	print('negative')
+
 elif ten > 0:
 	print('positive')
+	if ten < 50:
+		print('less than 50')
 else:
 	print('zero')
 
