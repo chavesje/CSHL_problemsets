69bbd41c0aac43deb5cbde5abb026d3c67294be1
jch <jch@pfb24.cshl.edu>
2018-10-16 11:30:18 -0400

    added favorite acitivy

--- name.py
+++ name.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 my_name = "julie chaves"
 my_favorite_color = "blue"
-print(my_name , my_favorite_color)
+my_favorite_activity = "running"
+print(my_name , my_favorite_color, my_favorite_activity)
+
 
