4b5c8308aabaf1e9a2256a9349dec1cb0a34f924
jch <jch@pfb24.cshl.edu>
2018-10-16 11:33:03 -0400

    added favorite animal

--- name.py
+++ name.py
@@ -2,6 +2,7 @@
 my_name = "julie chaves"
 my_favorite_color = "blue"
 my_favorite_activity = "running"
-print(my_name , my_favorite_color, my_favorite_activity)
+my_favorite_animal = "jerry"
+print(my_name , my_favorite_color, my_favorite_activity , my_favorite_animal)
 
 
