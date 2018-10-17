5ee5943c025e995280f5d35cb3a5b56cfdf646cf
jch <jch@pfb24.cshl.edu>
2018-10-17 11:57:15 -0400

    nucleotide counting function added

---
+++ pythonIIIVI.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+DNA_two = sys.argv[1]
+
+
+
+DNA_two.count ('A')
+DNA_two.count ('T')
+DNA_two.count ('G')
+DNA_two.count ('C')
+
+print(DNA_two.count ('A'))
+print(DNA_two.count ('T'))
+print(DNA_two.count ('G'))
+print(DNA_two.count ('C'))
+
+
