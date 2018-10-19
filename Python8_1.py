960377cef6e1661b7a7ca3753960ae75addd1e97
jch <jch@pfb24.cshl.edu>
2018-10-19 15:37:08 -0400

    fasta parser

--- Python8_1.py
+++ Python8_1.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
-fasta = open('Python_08.fasta', 'r')
+#fasta = open('Python_08.fasta', 'r')
+fasta = open('simplefasta.fa', 'r')
+
+fasta_dict = {}
 
 for line in fasta:
-	if '>' in line:
-		print("gene name:",line)
-
+	line=line.rstrip()
+	if line.startswith('>'):
+		header = line
+		fasta_dict[header]= 'nothing yet'
 	else:
-		print("sequence:",line)
+		seq = line
+		fasta_dict[header]= seq
+
+
+print(fasta_dict)		
 
 fasta.close()
